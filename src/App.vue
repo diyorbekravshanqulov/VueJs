86077efc7504904b24be51a3c31a0f9707d76a00
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-05-28 00:50:23 +0500

    complate

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { ref } from 'vue';
 import Header from "./components/Header.vue";
 import Top_side from "./components/Top_side.vue";
 import Category from "./components/category.vue";
 import Menu from "./components/Menu.vue";
 import Flowers from "./components/Flowers.vue";
 import Pagination from "./components/pagination.vue";
+import Info from "./components/Info.vue";
+
+const isActive = ref(false);
 </script>
 
 <template>
@@ -16,9 +20,43 @@ import Pagination from "./components/pagination.vue";
     </div>
     <div class="flex w-screen px-[10%] gap-[50px]">
       <Category class="w-[calc(100%/4+5%)]" />
-    <Flowers></Flowers>
+      <Flowers />
     </div>
 
     <Pagination />
+    <Info />
+  </div>
+
+  <div class="bg-black/55 fixed inset-0"></div>
+
+  <div class="fixed top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[500px] h-[600px] bg-white px-3">
+    <button @click="isActive = false" class="text-[30px] w-[100%] text-end font-medium">&times;</button>
+    <div class="flex flex-wrap items-center justify-center divide-[#999] mt-[10px]">
+      <button :class="{ 'text-[#46A358]': !isActive }" class="font-medium text-2xl text-center w-[94px]" @click="isActive = false">Login</button>
+      <button :class="{ 'text-[#46A358]': isActive }" class="font-medium text-2xl text-center w-[120px]" @click="isActive = true">Register</button>
+
+      <div v-if="!isActive" class="flex flex-wrap">
+        <p class="mt-[50px] text-center text-[#3D3D3D] text-[13px] w-[100%]">Enter your username and password to login.</p>
+        <p class="mt-[26px] text-center text-[#A5A5A5] text-[14px] w-[100%]">almamun_uxui@outlook.com</p>
+        <input placeholder="Password" type="password" name="" id="" class="px-[14px] py-[10px]  border border-[#46A358] placeholder:text-[#A5A5A5] rounded-md mx-auto w-[60%] mt-[30px]">
+        <p class="mt-[14px] text-[#46A358] leading-[14px] w-[60%] text-right mx-auto">Forgot Password?</p>
+        <button class="px-[129px] py-[14px] text-[16px] font-medium text-[#fff] border rounded-md bg-[#46A358] mt-[27px] w-[60%] mx-auto">Login</button>
+        <p class="mt-[36px] text-[13px] w-[60%] mx-auto text-center">Or login with</p>
+        <p class="mt-[26px] text-[13px] w-[60%] mx-auto text-center">Login with Google</p>
+        <p class="mt-[26px] text-[13px] w-[60%] mx-auto text-center">Login with Facebook</p>
+      </div>
+
+      <div v-else class="flex flex-wrap">
+        <p class="mt-[20px] text-center text-[#3D3D3D] text-[13px] w-[100%]">Enter your username and password to login.</p>
+        <input placeholder="Username" type="text" name="" id="" class="px-[14px] py-[10px] placeholder:text-[#A5A5A5]  border border-[#46A358] rounded-md mx-auto w-[60%] mt-[14px]">
+        <input placeholder="Email" type="email" name="" id="" class="px-[14px] py-[10px] placeholder:text-[#A5A5A5]  border border-[#46A358] rounded-md mx-auto w-[60%] mt-4">
+        <input placeholder="Password" type="password" name="" id="" class="px-[14px] py-[10px] placeholder:text-[#A5A5A5]  border border-[#46A358] rounded-md mx-auto w-[60%] mt-4">
+        <input placeholder="Confirm Password" type="password" name="" id="" class="px-[14px] py-[10px] placeholder:text-[#A5A5A5]  border border-[#46A358] rounded-md mx-auto w-[60%] mt-4">
+        <button class="px-[129px] py-[14px] text-[16px] font-medium text-[#fff] border rounded-md bg-[#46A358] mt-[27px] w-[60%] mx-auto">Register</button>
+        <p class="mt-[26px] text-[13px] w-[60%] mx-auto text-center">Or register with</p>
+        <p class="mt-[20px] text-[13px] w-[60%] mx-auto text-center">Register with Google</p>
+        <p class="mt-[20px] text-[13px] w-[60%] mx-auto text-center">Register with Facebook</p>
+      </div>
+    </div>
   </div>
 </template>
---
+++ src/components/Info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container flex gap-[28px]">
+    <div v-for="item in 2" :key="item" class="flex gap-[50px]">
+      <img src="../../public/info_flower.svg" alt="img" />
+      <div class="flex flex-wrap items-center justify-end">
+        <h3 class="text-2xl font-medium ">Lorem, ipsum dolor.</h3>
+        <p class="text-[#999] leading-7 ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic earum at
+          perferendis! Beatae, ducimus veritatis?
+        </p>
+        <button class="mr-5 px-7 py-2 bg-green-600 rounded-md text-white font-semibold">Find more -></button>
+      </div>
+    </div>
+  </div>
+</template>
