996553f3322edd28258e1cd98aa5e67738929679
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-03 21:54:16 +0500

    complate

--- src/pages/not-found.vue
+++ src/pages/not-found.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>
-        <h1 class="text-7xl text-center mt-10">404 Not page found</h1>
-    </div>
+  <div class="flex h-screen w-screen items-center justify-center bg-green-200">
+    <h1 class="text-9xl font-mono text-center mt-10 text-gray-50" style="text-shadow: 
+      2px 2px 0px #000, 
+      -2px -2px 0px #000, 
+      2px -2px 0px #000, 
+      -2px 2px 0px #000, 
+      2px 2px 0px #000, 
+      -2px -2px 0px #000, 
+      2px -2px 0px #000, 
+      -2px 2px 0px #000;">
+      404 Page Not Found
+    </h1>
+  </div>
 </template>
 
 <script setup>
-
 </script>
